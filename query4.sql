893f3b86b1a418e15fd2d13ab46d04aa98ae85fe
Cris Motinha <cristinamotinham@gmail.com>
2018-10-27 22:41:26 -0300

    Adiciona indice para query 4

---
+++ query4.sql
@@ -0,0 +1,7 @@
+select o_orderpriority, count(*) as order_count from ORDERS where o_orderdate >= date '1995-01-01' and o_orderdate < date '1995-01-01' + interval '3' month and exists (select * from LINEITEM where l_orderkey = o_orderkey and l_commitdate < l_receiptdate) group by o_orderpriority order by o_orderpriority;
+
+create index o_orderdate_idx on ORDERS(o_orderdate) -- foi de full table scan pra index range scan
+
+-- o indice "o_orderpriority_idx on ORDERS(o_orderpriority)" melhorou o group by, porém continuava fazendo full table scan
+-- Quando o "o_orderdate_idx on ORDERS(o_orderdate)" foi criado, foi de full table scan para index range scan
+-- e o group by voltou a ficar ruim, mas ele estava agrupando muito menos resultado agora, então compensou ficar somente com o indice pela orderdate mesmo.

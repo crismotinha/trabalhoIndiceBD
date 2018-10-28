b252d12fff8ae6e592db3c8bb5366691fe0bfff6
Cris Motinha <cristinamotinham@gmail.com>
2018-10-27 22:42:53 -0300

    adiciona os tempos que esqueci de botar antes

--- query4.sql
+++ query4.sql
@@ -1,7 +1,7 @@
 select o_orderpriority, count(*) as order_count from ORDERS where o_orderdate >= date '1995-01-01' and o_orderdate < date '1995-01-01' + interval '3' month and exists (select * from LINEITEM where l_orderkey = o_orderkey and l_commitdate < l_receiptdate) group by o_orderpriority order by o_orderpriority;
-
+-- tempo anterior: 0:00:0,81727314
 create index o_orderdate_idx on ORDERS(o_orderdate) -- foi de full table scan pra index range scan
-
+-- tempo depois: 0:00:0,37728715
 -- o indice "o_orderpriority_idx on ORDERS(o_orderpriority)" melhorou o group by, porém continuava fazendo full table scan
 -- Quando o "o_orderdate_idx on ORDERS(o_orderdate)" foi criado, foi de full table scan para index range scan
 -- e o group by voltou a ficar ruim, mas ele estava agrupando muito menos resultado agora, então compensou ficar somente com o indice pela orderdate mesmo.
